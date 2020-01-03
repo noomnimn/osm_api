89b730c6c625c59d0dcbc696466e0a74abcc1e1d
รัชนีกร รินทมาตย์ <osm007@srisangworn.local>
2020-01-03 11:47:30 +0700

    03/01/2020

---
+++ controllers/update.js
@@ -0,0 +1,52 @@
+exports.updateStatus = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    var { body } = req
+    var post = {
+        status: body.status ? body.status : null,
+        date_plan: body.date_plan ? body.date_plan : null,
+        date_dt: body.date_dt ? body.date_dt : null,
+        date_st: body.date_st ? body.date_st : null,
+        date_mn: body.date_mn ? body.date_mn : null,
+    }
+    req.getConnection(function (err, connection) {
+        connection.query("update requirements set ? where requirement_id =?", [post, id], function (err, results) {
+            if (err) return next(err)
+            res.send({ status: 'ok', results })
+        })
+    })
+}
+
+exports.updateMoney = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    var { body } = req
+    var post = {
+        m_date: body.m_date ? body.m_date : null,
+        money: body.money ? body.money : null,
+    }
+    req.getConnection(function (err, connection) {
+        connection.query("update requirements set ? where requirement_id =?", [post, id], function (err, results) {
+            if (err) return next(err)
+            res.send({ status: 'ok', results })
+        })
+    })
+}
+
+exports.updateStock = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    var { body } = req
+    var post = {
+        agreement_id: body.agreement_id ? body.agreement_id : null,
+        date_po: body.date_po ? body.date_po : null,
+        date_limit: body.date_limit ? body.date_limit : null,
+        date_completed: body.date_completed ? body.date_completed : null,
+        money_st: body.money_st ? body.money_st : null,
+        store: body.store ? body.store : null,
+    }
+    req.getConnection(function (err, connection) {
+        connection.query("update requirements set ? where requirement_id =?", [post, id], function (err, results) {
+            if (err) return next(err)
+            res.send({ status: 'ok', results })
+        })
+    })
+}
+
---
+++ routes.js
@@ -0,0 +1,87 @@
+const passport = require('passport')
+const passportService = require('./service/passport')
+const requireSignin = passport.authenticate('local', { session: false })
+const requireAuth = passport.authenticate('jwt', { session: false })
+const users = require('./controllers/Users')
+const user = require('./controllers/user')
+const plan = require('./controllers/plan')
+const dept = require('./controllers/Dept')
+const RegistOriginal = require('./controllers/Regist/RegistOriginal')
+const RegistStu = require('./controllers/Regist/RegistStu')
+const RegistTrain = require('./controllers/Regist/registTrain')
+const Schedule = require('./controllers/Schedule/Schedule')
+const Leave = require('./controllers/Leave/LeaveForm')
+const requirement = require('./controllers/requirement')
+const update = require('./controllers/update')
+const archives = require('./controllers/archives')
+const project = require('./controllers/goal')
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.send("<h1 style='text-align:center;margin-top:150px; '>PSoffice Api</h1>")
+    })
+    app.post('/signin', requireSignin, users.signin)
+
+    // user
+    app.get('/users', users.findAll)
+    app.get('/users/:limit/:offset/:sort_by/:fields/:text', users.findallpagination)
+    app.post('/users', users.create)
+    app.get('/users/findbyid/:id', users.findById)
+    app.put('/users/:id', users.update)
+    app.delete('/users/:id', users.delete)
+    app.get('/users-profile/:id', users.findProfile)
+    app.get('/hash-password', users.hashPassword)
+
+    // members
+    app.delete('/api/v1/users/:id', user.deleteUser)
+    app.get('/api/v1/users/data/:id', user.findDataById)
+    app.get('/api/v1/users/findall', user.findAll)
+    app.put('/api/v1/users/:id', user.updateUser)
+    app.post('/api/v1/users', user.createUser)
+    app.get('/api/v1/users/findallpagination/:limit/:offset/:sort_by/:fields/:text', users.findallpagination)
+
+    // PLAN
+    app.get('/api/v1/plan/getDeptList', dept.findDeptList)
+    app.get('/api/v1/plan/findall', plan.findAll)
+    app.get('/api/v1/plan/findallpagination/:limit/:offset/:text', plan.findallpagination)
+    app.get('/api/v1/plan/getbyId/:id', plan.findPlanById)
+    app.post('/api/v1/plan', plan.createPlan)
+    app.delete('/api/v1/plan/:id', plan.deletePlan)
+    app.put('/api/v1/plan/:id', plan.updatePlan)
+
+    // Requirement
+    app.get('/api/v1/requirement/getbyId/:id', requirement.findReqById)
+    app.get('/api/v1/requirement/findall/:limit/:offset/:text', requirement.findAll)
+    app.post('/api/v1/requirement', requirement.createReq)
+    app.delete('/api/v1/requirement/:id', requirement.deleteReq)
+    app.put('/api/v1/requirement/:id', requirement.updateReq)
+    //app.put('/api/v1/requirement/updatestatus/:id', requirement.updateReq)
+    app.get('/api/v1/requirement/exportdata/:status', requirement.exportData)
+
+    // update/id
+    app.put('/api/v1/requirement/updatestatus/:id', update.updateStatus)
+    app.put('/api/v1/requirement/updatemoney/:id', update.updateMoney)
+    app.put('/api/v1/requirement/updatestock/:id', update.updateStock)
+
+    // app.post('/upload', upload.upload)
+
+        // Arc
+       // app.get('/api/v1/plan/getDeptList', dept.findDeptList)
+       
+    app.get('/api/v1/archives/findall/:limit/:offset/:text', archives.findAll)
+    app.get('/api/v1/archives/findall', archives.findAll)
+    app.get('/api/v1/archives/findallpagination/:limit/:offset/:text', archives.findallpagination)
+    app.get('/api/v1/archives/getbyId/:id', archives.findArcById)
+    app.post('/api/v1/archives', archives.createArc)
+    app.delete('/api/v1/archives/:id', archives.deleteArc)
+    app.put('/api/v1/archives/:id', archives.updateArc)
+
+   // PLAN
+   app.get('/api/v1/project/findall', project.findAll)
+   app.get('/api/v1/project/findallpagination/:limit/:offset/:text', project.findallpagination)
+   app.get('/api/v1/project/getbyId/:id', project.findById)
+   app.post('/api/v1/project', project.createGoal)
+   app.delete('/api/v1/project/:id', project.deleteGoal)
+   app.put('/api/v1/project/:id', project.updateGoal)
+    
+}
